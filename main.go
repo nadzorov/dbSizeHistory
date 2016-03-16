60173227deb546650acb1c03c359b1fee3d3fe37
nadzorov <nadzorov@mail.ru>
2016-03-17 00:09:06 +0300

    parse all csv files in a directory using filepath.Walk

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -27,11 +28,18 @@ func parseCsvFile(file *os.File) []tableSpace {
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		// debug
 		// fmt.Printf("%#v \n", record)
 		// for i, col := range record {
 		// 	fmt.Printf("%#v %#v \n", i, strings.TrimSpace(col))
 		// }
+
+		// skip headers
+		if strings.TrimSpace(record[1]) == "DBNAME" {
+			continue
+		}
+
 		ts = append(ts, tableSpace{
 			dbName:      strings.TrimSpace(record[1]),
 			tsName:      strings.TrimSpace(record[2]),
@@ -42,9 +50,51 @@ func parseCsvFile(file *os.File) []tableSpace {
 	return ts
 }
 
-func main() {
+// called from filepath.Walk
+func parseCsvFileWithOpen(fileName string) []tableSpace {
 	// Open file
-	file, err := os.Open("./data/cftwork.log")
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	ts := []tableSpace{}
+	csvReader := csv.NewReader(file)
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// debug
+		// fmt.Printf("%#v \n", record)
+		// for i, col := range record {
+		// 	fmt.Printf("%#v %#v \n", i, strings.TrimSpace(col))
+		// }
+
+		// skip headers
+		if strings.TrimSpace(record[1]) == "DBNAME" {
+			continue
+		}
+
+		ts = append(ts, tableSpace{
+			dbName:      strings.TrimSpace(record[1]),
+			tsName:      strings.TrimSpace(record[2]),
+			gbFreeOfMax: strings.TrimSpace(record[9]),
+			date:        strings.TrimSpace(record[0]),
+		})
+	}
+	return ts
+}
+
+// Run parseCsvFile with one hardcoded file
+func testParseCsvFile() {
+	// Open file
+	file, err := os.Open("data/cftwork.log")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -54,5 +104,46 @@ func main() {
 	for _, ts := range allTs {
 		fmt.Printf("%#v \n", ts)
 	}
+}
+
+func testFilepathWalk() {
+	allTs := []tableSpace{}
+	filepath.Walk("./data", func(path string, fi os.FileInfo, err error) error {
+		// skip directories
+		if fi.IsDir() {
+			return nil
+		}
+
+		// TODO: skip all hidden files or read only *.log files
+		if path == "data/.DS_Store" {
+			return nil
+		}
+
+		// TODO: why contains "," in PCT_FREE_OF_MAX ?
+		if path == "data/INFORMER.log" {
+			return nil
+		}
+		// TODO: why contains "," in PCT_FREE_OF_MAX ?
+		if path == "data/SMSGATE.log" {
+			return nil
+		}
+
+		// debug
+		// fmt.Printf("%#v \n", path)
+
+		allTs = append(allTs, parseCsvFileWithOpen(path)...)
+		return nil
+
+	})
+
+	for _, ts := range allTs {
+		fmt.Printf("%#v \n", ts)
+	}
+}
+
+func main() {
+
+	// testParseCsvFile()
+	testFilepathWalk()
 
 }
